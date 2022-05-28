06a42344438b3104d086045c403a7965b12df263
tfkhdyt <tfkhdyt@pm.me>
2022-05-28 17:14:11 +0700

    add coffeemachine and module quiz

---
+++ dasar-pemrograman-javascript/CoffeeMachine/index.js
@@ -0,0 +1,31 @@
+// console.log("Menyalakan mesin kopi");
+// console.log("Menggiling biji kopi");
+// console.log("Memanaskan air");
+// console.log("Mencampurkan air dan kopi");
+// console.log("Menuangkan kopi ke dalam gelas");
+// console.log("Menuangkan susu ke dalam gelas");
+// console.log("Kopi Anda sudah siap!");
+
+// const { coffeeStock, isCoffeeMachineReady } = require('./state');
+import { coffeeStock as stock, isCoffeeMachineReady } from './state.js';
+
+console.log(stock);
+console.log(isCoffeeMachineReady);
+
+// const makeCoffee = (type, miligrams) => {
+//   if (coffeeStock[type] >= miligrams) {
+//     console.log('Kopi berhasil dibuat!');
+//   } else {
+//     console.log('Biji kopi habis!');
+//   }
+// };
+
+// makeCoffee('robusta', 80);
+
+// const displayStock = (stock) => {
+//   for (const type in stock) {
+//     console.log(type);
+//   }
+// };
+
+// displayStock(coffeeStock);
---
+++ dasar-pemrograman-javascript/CoffeeMachine/state.js
@@ -0,0 +1,14 @@
+export const coffeeStock = {
+  arabica: 100,
+  robusta: 150,
+  liberica: 200,
+};
+
+export const isCoffeeMachineReady = true;
+
+// module.exports = {
+//   coffeeStock,
+//   isCoffeeMachineReady,
+// };
+
+// export default coffeeStock;
