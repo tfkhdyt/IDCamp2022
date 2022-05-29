2fab6b3dd27cdb25b0084c3af119ccd392bf81cd
tfkhdyt <tfkhdyt@pm.me>
2022-05-29 20:30:47 +0700

    add method to server.js in "nodejs-web-server"

--- membuat-aplikasi-backend/nodejs-web-server/server.js
+++ membuat-aplikasi-backend/nodejs-web-server/server.js
@@ -7,7 +7,25 @@ const HOST = 'localhost';
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
   response.statusCode = 200;
-  response.end('<h1>Halo HTTP Server!</h1>');
+
+  const { method } = request;
+
+  switch (method) {
+    case 'GET':
+      response.end('<h1>Hello!</h1>');
+      break;
+    case 'POST':
+      response.end('<h1>Hai!</h1>');
+      break;
+    case 'PUT':
+      response.end('<h1>Bonjour!</h1>');
+      break;
+    case 'DELETE':
+      response.end('<h1>Salam!</h1>');
+      break;
+    default:
+      console.error('Method tidak valid!');
+  }
 };
 
 const server = http.createServer(requestListener);
