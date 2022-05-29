752d1d5f1413664de7ab64401feb54a9fb448674
tfkhdyt <tfkhdyt@pm.me>
2022-05-30 06:21:19 +0700

    move routes to seperate file

---
+++ membuat-aplikasi-backend/hapi-web-server/routes.js
@@ -0,0 +1,39 @@
+const routes = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Homepage';
+    },
+  },
+  {
+    method: '*',
+    path: '/',
+    handler: () => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (request, h) => {
+      return 'About page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: () => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => {
+      return 'Halaman tidak ditemukan';
+    },
+  },
+];
+
+module.exports = routes;
