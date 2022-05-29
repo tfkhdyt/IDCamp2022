22a05376d354191558c93ab265b43862b5140aac
tfkhdyt <tfkhdyt@pm.me>
2022-05-29 20:05:45 +0700

    add "writable-stream" and "stream"

---
+++ membuat-aplikasi-backend/nodejs-basic/stream/index.js
@@ -0,0 +1,24 @@
+/**
+ * TODO:
+ * Buatlah program untuk membaca teks input.txt dan menuliskannya ulang pada berkas output.txt
+ * menggunakan teknik readable stream dan writable stream.
+ */
+const fs = require('fs');
+
+const readableStream = fs.createReadStream('./input.txt', {
+  highWaterMark: 15,
+});
+
+const writableStream = fs.createWriteStream('./output.txt');
+
+readableStream.on('readable', () => {
+  try {
+    writableStream.write(`${readableStream.read()}\n`);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+readableStream.on('end', () => {
+  writableStream.end();
+});
