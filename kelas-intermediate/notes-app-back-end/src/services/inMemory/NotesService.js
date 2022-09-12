1afb328c6fa9f6eb51764593b5d207d3e30c225f
tfkhdyt <tfkhdyt@pm.me>
2022-09-12 11:05:44 +0700

    feat(kelas intermediate): implement data validation

--- kelas-intermediate/notes-app-back-end/src/services/inMemory/NotesService.js
+++ kelas-intermediate/notes-app-back-end/src/services/inMemory/NotesService.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -24,7 +26,7 @@ class NotesService {
 
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
-    if (!isSuccess) throw new Error('Catatan gagal ditambahkan');
+    if (!isSuccess) throw new InvariantError('Catatan gagal ditambahkan');
 
     return id;
   }
@@ -36,7 +38,7 @@ class NotesService {
   getNoteById(id) {
     const note = this._notes.find((n) => n.id === id);
     if (!note) {
-      throw new Error('Catatan tidak ditemukan');
+      throw new NotFoundError('Catatan tidak ditemukan');
     }
     return note;
   }
@@ -44,7 +46,7 @@ class NotesService {
   editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((note) => note.id === id);
     if (index === -1) {
-      throw new Error('Gagal memperbarui catatan, ID tidak ditemukan');
+      throw new NotFoundError('Gagal memperbarui catatan, ID tidak ditemukan');
     }
 
     const updatedAt = new Date().toISOString();
@@ -61,7 +63,7 @@ class NotesService {
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
     if (index === -1) {
-      throw new Error('Catatan gagal dihapus, ID tidak ditemukan');
+      throw new NotFoundError('Catatan gagal dihapus, ID tidak ditemukan');
     }
     this._notes.splice(index, 1);
   }
