9c01dc1d3c7e714e6e758cd1de57c33f15c40c35
tfkhdyt <tfkhdyt@pm.me>
2022-09-12 12:03:02 +0700

    feat(notes api): setup dotenv

--- kelas-intermediate/notes-app-back-end/src/server.js
+++ kelas-intermediate/notes-app-back-end/src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 
 const notes = require('./api/notes');
@@ -8,8 +10,8 @@ const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
