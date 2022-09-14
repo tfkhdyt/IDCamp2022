b959a23b0b0cedcd79a058e71b5137ed928cafde
tfkhdyt <tfkhdyt@pm.me>
2022-09-14 15:59:01 +0700

    feat(submission 1): add songs handler, service and routes

---
+++ kelas-intermediate/Submission/1. OpenMusic/src/api/songs/handler.js
@@ -0,0 +1,62 @@
+const handleError = require('../../utils/handleError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongsHandler = this.getSongsHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+  }
+
+  async postSongHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const { title, year, genre, performer, duration, albumId } =
+        request.payload;
+      const songId = await this._service.addSong({
+        title,
+        year,
+        genre,
+        performer,
+        duration,
+        albumId,
+      });
+
+      const response = h.response({
+        status: 'success',
+        data: { songId },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      return handleError(error, h);
+    }
+  }
+
+  async getSongsHandler() {
+    const songs = await this._service.getSongs();
+    return {
+      status: 'success',
+      data: {
+        songs,
+      },
+    };
+  }
+
+  async getSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const song = await this._service.getSongById(id);
+      return {
+        status: 'success',
+        data: { song },
+      };
+    } catch (error) {
+      return handleError(error, h);
+    }
+  }
+}
+
+module.exports = SongsHandler;
---
+++ kelas-intermediate/Submission/1. OpenMusic/src/services/postgres/SongsService.js
@@ -0,0 +1,53 @@
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+
+class SongsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong({ title, year, genre, performer, duration, albumId }) {
+    const id = 'song-' + nanoid(16);
+
+    const query = {
+      text: 'INSERT INTO songs VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id',
+      values: [id, title, year, genre, performer, duration, albumId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSongs() {
+    const result = await this._pool.query('SELECT * FROM songs');
+    return result.rows.map((song) => ({
+      id: song.id,
+      title: song.title,
+      performer: song.performer,
+    }));
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM songs WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
+}
+
+module.exports = SongsService;
