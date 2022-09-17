6b4a2a509c8367a13893fa391f91c7cd57c1c0d1
tfkhdyt <tfkhdyt@pm.me>
2022-09-17 18:22:09 +0700

    feat(handler): add auth handler

---
+++ kelas-intermediate/notes-app-back-end/src/api/authentications/handler.js
@@ -0,0 +1,79 @@
+const { default: autoBind } = require('auto-bind');
+const failResponse = require('../../utils/responses/fail');
+const successResponse = require('../../utils/responses/success');
+
+/* eslint-disable no-underscore-dangle */
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postAuthenticationHandler(request, h) {
+    try {
+      this._validator.validatePostAuthenticationPayload(request.payload);
+
+      const { username, password } = request.payload;
+      const id = await this._usersService.verifyUserCredential(
+        username,
+        password
+      );
+
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+      const refreshToken = this._tokenManager.generateRefreshToken({ id });
+
+      await this._authenticationsService.addRefreshToken(refreshToken);
+
+      return successResponse(h, {
+        message: 'Authentication berhasil ditambahkan',
+        data: {
+          accessToken,
+          refreshToken,
+        },
+        code: 201,
+      });
+    } catch (error) {
+      return failResponse(error, h);
+    }
+  }
+
+  async putAuthenticationHandler(request, h) {
+    try {
+      this._validator.validatePutAuthenticationPayload(request.payload);
+
+      const { refreshToken } = request.payload;
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      const { id } = this._tokenManager.verifyRefreshToken(refreshToken);
+      const accessToken = this._tokenManager.generateAccessToken({ id });
+
+      return successResponse(h, {
+        message: 'Access token berhasil diperbarui',
+        data: { accessToken },
+      });
+    } catch (error) {
+      return failResponse(error, h);
+    }
+  }
+
+  async deleteAuthenticationHandler(request, h) {
+    try {
+      this._validator.validateDeleteAuthenticationPayload(request.payload);
+
+      const { refreshToken } = request.payload;
+      await this._authenticationsService.verifyRefreshToken(refreshToken);
+      await this._authenticationsService.deleteRefreshToken(refreshToken);
+
+      return successResponse(h, {
+        message: 'Refresh token berhasil dihapus',
+      });
+    } catch (error) {
+      return failResponse(error, h);
+    }
+  }
+}
+
+module.exports = AuthenticationsHandler;
