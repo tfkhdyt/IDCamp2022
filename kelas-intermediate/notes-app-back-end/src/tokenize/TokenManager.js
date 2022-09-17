864b9129c3483c4a50fe38293a62e28ef8b1fc43
tfkhdyt <tfkhdyt@pm.me>
2022-09-17 17:45:38 +0700

    feat(token manager): add verify refresh token method

--- kelas-intermediate/notes-app-back-end/src/tokenize/TokenManager.js
+++ kelas-intermediate/notes-app-back-end/src/tokenize/TokenManager.js
@@ -1,10 +1,24 @@
 const Jwt = require('@hapi/jwt');
 
+const InvariantError = require('../exceptions/InvariantError');
+
 const TokenManager = {
+  refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
   generateAccessToken: (payload) =>
     Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
   generateRefreshToken: (payload) =>
-    Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+    Jwt.token.generate(payload, this.refreshTokenKey),
+  verifyRefreshToken(refreshToken) {
+    try {
+      const artifacts = Jwt.token.decode(refreshToken);
+      Jwt.token.verifySignature(artifacts, this.refreshTokenKey);
+
+      const { payload } = artifacts.decoded;
+      return payload;
+    } catch (error) {
+      throw new InvariantError('Refresh token tidak valid');
+    }
+  },
 };
 
 module.exports = TokenManager;
