cb2192260ee659f510e6a738c6e23f1a84b11e84
tfkhdyt <tfkhdyt@pm.me>
2022-09-18 17:18:00 +0700

    feat: register auth strategy

--- kelas-intermediate/notes-app-back-end/src/server.js
+++ kelas-intermediate/notes-app-back-end/src/server.js
@@ -2,16 +2,23 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 
+const Jwt = require('@hapi/jwt');
 const notes = require('./api/notes');
 const users = require('./api/users');
+const authentications = require('./api/authentications');
 const NotesService = require('./services/postgres/NotesService');
 const UsersService = require('./services/postgres/UsersService');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
 const NotesValidator = require('./validator/notes');
 const UsersValidator = require('./validator/users');
+const AuthenticationsValidator = require('./validator/authentications');
+
+const TokenManager = require('./tokenize/TokenManager');
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -21,6 +28,29 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
@@ -36,6 +66,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
