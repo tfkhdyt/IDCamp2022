c1f5a5c45c394299836472d2f530770f5c4c5585
tfkhdyt <tfkhdyt@pm.me>
2022-09-26 05:50:07 +0700

    feat(submission): add add user feature

---
+++ kelas-intermediate/Submission/openmusic/src/api/users/handler.js
@@ -0,0 +1,31 @@
+const autoBind = require('auto-bind');
+
+const successResponse = require('../../utils/responses/success');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postUserHandler(request, h) {
+    this._validator.validateUserPayload(request.payload);
+
+    const { username, password, fullname } = request.payload;
+    const userId = await this._service.addUser({
+      username,
+      password,
+      fullname,
+    });
+
+    return successResponse(h, {
+      message: 'User berhasil ditambahkan',
+      data: { userId },
+      statusCode: 201,
+    });
+  }
+}
+
+module.exports = UsersHandler;
--- kelas-intermediate/Submission/openmusic/src/server.js
+++ kelas-intermediate/Submission/openmusic/src/server.js
@@ -4,17 +4,25 @@ const Hapi = require('@hapi/hapi');
 
 const albums = require('./api/albums');
 const songs = require('./api/songs');
+const users = require('./api/users');
+
 const ClientError = require('./exceptions/ClientError');
+
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsService = require('./services/postgres/SongsService');
+const UsersService = require('./services/postgres/UsersService');
+
 const errorResponse = require('./utils/responses/error');
 const failResponse = require('./utils/responses/fail');
+
 const AlbumsValidator = require('./validator/albums');
 const SongsValidator = require('./validator/songs');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -39,6 +47,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator,
+      },
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
       },
     },
   ]);
