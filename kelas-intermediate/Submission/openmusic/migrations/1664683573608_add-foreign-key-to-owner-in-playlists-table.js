0fcf33fcead799e3ed7d5abcc2291cffe65a4dac
tfkhdyt <tfkhdyt@pm.me>
2022-10-02 11:19:43 +0700

    feat(migrations): add foreign key to playlists table

---
+++ kelas-intermediate/Submission/openmusic/migrations/1664683573608_add-foreign-key-to-owner-in-playlists-table.js
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.addConstraint(
+    'playlists',
+    'fk_playlists.owner_users.id',
+    'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE'
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlists', 'fk_playlists.owner_users.id');
+};
