e7b3987405b5f6a0aa9e28454ee30ccdfb10440c
tfkhdyt <tfkhdyt@pm.me>
2022-10-09 17:22:40 +0700

    feat(plugins): create uploads plugin

---
+++ kelas-intermediate/notes-app-back-end/src/api/uploads/handler.js
@@ -0,0 +1,28 @@
+const autoBind = require('auto-bind');
+
+const successResponse = require('../../utils/responses/success');
+
+class UploadsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postUploadImageHandler(request, h) {
+    const { data } = request.payload;
+    this._validator.validateImageHeaders(data.hapi.headers);
+
+    const filename = await this._service.writeFile(data, data.hapi);
+
+    return successResponse(h, {
+      data: {
+        fileLocation: `http://${process.env.HOST}:${process.env.PORT}/uploads/images/${filename}`,
+      },
+      code: 201,
+    });
+  }
+}
+
+module.exports = UploadsHandler;
