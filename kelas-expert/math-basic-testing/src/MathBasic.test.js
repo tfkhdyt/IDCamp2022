151fd9c44d3aef418e76d168196d5a7625228dc6
tfkhdyt <tfkhdyt@proton.me>
2022-11-29 06:01:25 +0700

    feat(mathbasic test): add testing for "mathbasic" object and "add" method

---
+++ kelas-expert/math-basic-testing/src/MathBasic.test.js
@@ -0,0 +1,36 @@
+const MathBasic = require('./MathBasic');
+
+describe('A MathBasic', () => {
+  it('should contains add, substract, multiply, and divide function', () => {
+    expect(MathBasic).toHaveProperty('add');
+    expect(MathBasic).toHaveProperty('substract');
+    expect(MathBasic).toHaveProperty('multiply');
+    expect(MathBasic).toHaveProperty('divide');
+
+    expect(MathBasic.add).toBeInstanceOf(Function);
+    expect(MathBasic.substract).toBeInstanceOf(Function);
+    expect(MathBasic.multiply).toBeInstanceOf(Function);
+    expect(MathBasic.divide).toBeInstanceOf(Function);
+  });
+});
+
+describe('An add function', () => {
+  it('should throw error when not given 2 parameter', () => {
+    expect(() => MathBasic.add()).toThrowError();
+    expect(() => MathBasic.add(1)).toThrowError();
+    expect(() => MathBasic.add(1, 2, 3)).toThrowError();
+    expect(() => MathBasic.add(1, 2, 3, 4)).toThrowError();
+  });
+
+  it('should throw error when given non-number parameters', () => {
+    expect(() => MathBasic.add('1', '2')).toThrowError();
+    expect(() => MathBasic.add(true, {})).toThrowError();
+    expect(() => MathBasic.add(null, false)).toThrowError();
+  });
+
+  it('should return a + b when given two number parameters', () => {
+    expect(MathBasic.add(2, 2)).toEqual(4);
+    expect(MathBasic.add(16, 8)).toEqual(24);
+    expect(MathBasic.add(3, 7)).toEqual(10);
+  });
+});
