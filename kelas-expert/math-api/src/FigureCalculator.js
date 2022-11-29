2a40ff68bc458a39f292b8d6e9fbcba342e6e57e
tfkhdyt <tfkhdyt@proton.me>
2022-11-29 20:47:46 +0700

    feat(figure calculator): add calculateRectangleArea method

--- kelas-expert/math-api/src/FigureCalculator.js
+++ kelas-expert/math-api/src/FigureCalculator.js
@@ -16,7 +16,24 @@ class FigureCalculator {
 
     return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
   }
-  calculateRectangleArea() {}
+
+  calculateRectangleArea(...args) {
+    if (args.length !== 2) {
+      throw new Error('fungsi hanya menerima 2 parameter');
+    }
+
+    const [length, width] = args;
+
+    if (
+      typeof length !== 'number' ||
+      typeof width !== 'number' ||
+      typeof width !== 'number'
+    ) {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    return this._mathBasic.multiply(length, width);
+  }
   calculateTrianglePerimeter() {}
   calculateTriangleArea() {}
 }
