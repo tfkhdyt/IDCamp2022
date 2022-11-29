4439c92fc541ae30fe2980aa6552e5516713687f
tfkhdyt <tfkhdyt@proton.me>
2022-11-29 07:08:06 +0700

    test(math basic): add unit testing for divide function

--- kelas-expert/math-basic-testing/src/MathBasic.test.js
+++ kelas-expert/math-basic-testing/src/MathBasic.test.js
@@ -76,3 +76,24 @@ describe('A multiply function', () => {
     expect(MathBasic.multiply(3, 7)).toEqual(21);
   });
 });
+
+describe('A divide function', () => {
+  it('should throw error when not given 2 parameter', () => {
+    expect(() => MathBasic.divide()).toThrowError();
+    expect(() => MathBasic.divide(1)).toThrowError();
+    expect(() => MathBasic.divide(1, 2, 3)).toThrowError();
+    expect(() => MathBasic.divide(1, 2, 3, 4)).toThrowError();
+  });
+
+  it('should throw error when given non-number parameters', () => {
+    expect(() => MathBasic.divide('1', '2')).toThrowError();
+    expect(() => MathBasic.divide(true, {})).toThrowError();
+    expect(() => MathBasic.divide(null, false)).toThrowError();
+  });
+
+  it('should return a / b when given two number parameters', () => {
+    expect(MathBasic.divide(2, 2)).toEqual(1);
+    expect(MathBasic.divide(16, 8)).toEqual(2);
+    expect(MathBasic.divide(30, 10)).toEqual(3);
+  });
+});
