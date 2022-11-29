bc11f36f81dcf233238e9672fa791758c01719eb
tfkhdyt <tfkhdyt@proton.me>
2022-11-29 07:59:41 +0700

    feat(figure calculator): add figure calculator class and calculate methods

---
+++ kelas-expert/math-api/src/FigureCalculator.js
@@ -0,0 +1,24 @@
+class FigureCalculator {
+  constructor(mathBasic) {
+    this._mathBasic = mathBasic;
+  }
+
+  calculateRectanglePerimeter(...args) {
+    if (args.length !== 2) {
+      throw new Error('fungsi hanya menerima dua parameter');
+    }
+
+    const [length, width] = args;
+
+    if (typeof length !== 'number' || typeof width !== 'number') {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    return this._mathBasic.multiply(2, this._mathBasic.add(length, width));
+  }
+  calculateRectangleArea() {}
+  calculateTrianglePerimeter() {}
+  calculateTriangleArea() {}
+}
+
+module.exports = FigureCalculator;
