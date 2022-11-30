9a8f22edef85de50b838257585bf44e7aefdadb2
tfkhdyt <tfkhdyt@proton.me>
2022-11-30 17:10:44 +0700

    feat(figure calculator): add calculateTriangleArea function

--- kelas-expert/math-api/src/FigureCalculator.js
+++ kelas-expert/math-api/src/FigureCalculator.js
@@ -40,9 +40,9 @@ class FigureCalculator {
     const [a, b, c] = args;
 
     if (
-      typeof a !== 'number'
-      || typeof b !== 'number'
-      || typeof c !== 'number'
+      typeof a !== 'number' ||
+      typeof b !== 'number' ||
+      typeof c !== 'number'
     ) {
       throw new Error('fungsi hanya menerima parameter number');
     }
@@ -51,7 +51,20 @@ class FigureCalculator {
     return this._mathBasic.add(sumOfAB, c);
   }
 
-  calculateTriangleArea() {}
+  calculateTriangleArea(...args) {
+    if (args.length !== 2) {
+      throw new Error('fungsi hanya menerima 2 parameter');
+    }
+
+    const [base, height] = args;
+
+    if (typeof base !== 'number' || typeof height !== 'number') {
+      throw new Error('fungsi hanya menerima parameter number');
+    }
+
+    const alasKaliTinggi = this._mathBasic.multiply(base, height);
+    return this._mathBasic.multiply(alasKaliTinggi, 0.5);
+  }
 }
 
 module.exports = FigureCalculator;
