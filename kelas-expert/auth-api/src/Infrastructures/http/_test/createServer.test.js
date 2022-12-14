848591b3a896b334d19d8231f2f753cbb2fe016e
tfkhdyt <tfkhdyt@proton.me>
2022-12-15 05:16:23 +0700

    test(infra/http): write test for createServer

---
+++ kelas-expert/auth-api/src/Infrastructures/http/_test/createServer.test.js
@@ -0,0 +1,40 @@
+const pool = require('../../database/postgres/pool');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+
+describe('HTTP server', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /users', () => {
+    it('should response 201 and persisted user', async () => {
+      // arrange
+      const requestPayload = {
+        username: 'dicoding',
+        password: 'secret',
+        fullname: 'Dicoding Indonesia',
+      };
+      const server = await createServer(container);
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedUser).toBeDefined();
+    });
+  });
+});
