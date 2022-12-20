4b8276351df47d0cf692feca6f7238b844dcf521
tfkhdyt <tfkhdyt@proton.me>
2022-12-20 12:10:23 +0700

    feat(test helpers): add ThreadsTableTestHelper

---
+++ kelas-expert/forum-api/tests/ThreadsTableTestHelper.js
@@ -0,0 +1,34 @@
+// istanbul ignore file
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'thread-123',
+    title = 'sebuah thread',
+    body = 'isi thread',
+    owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, DEFAULT, $4)',
+      values: [id, title, body, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows[0];
+  },
+
+  async cleanTable() {
+    await pool.query('TRUNCATE TABLE threads');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
