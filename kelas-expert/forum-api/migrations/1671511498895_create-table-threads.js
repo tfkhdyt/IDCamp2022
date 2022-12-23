6b2ef7f5ff3cad66d7658c7c1e8114ea058f7f98
tfkhdyt <tfkhdyt@proton.me>
2022-12-23 16:44:46 +0700

    fix(migrations): move date after owner field in "threads" table

--- kelas-expert/forum-api/migrations/1671511498895_create-table-threads.js
+++ kelas-expert/forum-api/migrations/1671511498895_create-table-threads.js
@@ -14,14 +14,14 @@ exports.up = (pgm) => {
       type: 'TEXT',
       notNull: true,
     },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
     date: {
       type: 'TIMESTAMP',
       notNull: true,
       default: pgm.func('current_timestamp'),
-    },
-    owner: {
-      type: 'VARCHAR(50)',
-      notNull: true,
     },
   });
 
