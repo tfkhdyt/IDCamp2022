21d09ab4835f2bdcf7083a9335c38561c44485c0
tfkhdyt <tfkhdyt@proton.me>
2022-12-29 09:10:26 +0700

    refactor(infra/http): move /threads endpoint testing to separate file

---
+++ kelas-expert/forum-api/src/Infrastructures/http/_test/threads.test.js
@@ -0,0 +1,144 @@
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const pool = require('../../database/postgres/pool');
+const createServer = require('../createServer');
+
+describe('/threads endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads', () => {
+    it('should response 201 and persisted thread', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      const server = await createServer(container);
+
+      const requestPayload = {
+        title: 'ini judul',
+        body: 'ini body',
+        owner: 'user-123',
+      };
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedThread).toBeDefined();
+    });
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      const requestPayload = {
+        title: 'ini judul',
+      };
+      const server = await createServer(container);
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada'
+      );
+    });
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      const requestPayload = {
+        title: 'ini judul',
+        body: true,
+      };
+      const server = await createServer(container);
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat thread baru karena tipe data tidak sesuai'
+      );
+    });
+
+    it('should response 400 when username more than 50 characters', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      const requestPayload = {
+        title:
+          'inijudulinijudulinijudulinijudulinijudulinijudulinijudulinijudulinijudul',
+        body: 'ini body',
+      };
+      const server = await createServer(container);
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat thread baru karena karakter title melebihi batas limit'
+      );
+    });
+  });
+});
