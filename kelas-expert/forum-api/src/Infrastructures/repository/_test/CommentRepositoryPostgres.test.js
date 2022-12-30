7a13b7e65b3981b2cc3c79ba41faea8b574e06eb
tfkhdyt <tfkhdyt@proton.me>
2022-12-30 18:29:03 +0700

    test(infra/repo): write test for CommentRepoPostgres

---
+++ kelas-expert/forum-api/src/Infrastructures/repository/_test/CommentRepositoryPostgres.test.js
@@ -0,0 +1,78 @@
+const CommentRepositoryPostgres = require('../CommentRepositoryPostgres');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const NewComment = require('../../../Domains/comments/entities/NewComment');
+const pool = require('../../database/postgres/pool');
+const AddedComment = require('../../../Domains/comments/entities/AddedComment');
+
+describe('CommentRepositoryPostgres', () => {
+  afterEach(async () => {
+    await CommentsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addComment function', () => {
+    it('should persist new comment', async () => {
+      // arrange
+      await UsersTableTestHelper.addUser({ username: 'tfkhdyt' });
+      await ThreadsTableTestHelper.addThread({ title: 'sebuah thread' });
+      const newComment = new NewComment({
+        content: 'ini konten',
+        owner: 'user-123',
+        threadId: 'thread-123',
+      });
+      const fakeIdGenerator = () => '123'; // stub
+      const commentRepositoryPostgres = new CommentRepositoryPostgres(
+        pool,
+        fakeIdGenerator
+      );
+
+      // action
+      await commentRepositoryPostgres.addComment(newComment);
+
+      // assert
+      const comments = await CommentsTableTestHelper.findCommentById(
+        'comment-123'
+      );
+      expect(comments).toHaveLength(1);
+    });
+
+    it('should return added comment correctly', async () => {
+      // arrange
+      await UsersTableTestHelper.addUser({ username: 'tfkhdyt' });
+      await ThreadsTableTestHelper.addThread({ title: 'sebuah thread' });
+      const newComment = new NewComment({
+        content: 'ini konten',
+        owner: 'user-123',
+        threadId: 'thread-123',
+      });
+      const fakeIdGenerator = () => '123'; // stub
+      const commentRepositoryPostgres = new CommentRepositoryPostgres(
+        pool,
+        fakeIdGenerator
+      );
+
+      // action
+      const addedComment = await commentRepositoryPostgres.addComment(
+        newComment
+      );
+
+      // assert
+      expect(addedComment).toMatchObject(
+        new AddedComment({
+          id: 'comment-123',
+          content: 'ini konten',
+          isDeleted: false,
+          owner: 'user-123',
+          threadId: 'thread-123',
+        })
+      );
+    });
+  });
+});
