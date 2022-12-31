e5cc1bbbfa03d47a5838eac19808f98684280461
tfkhdyt <tfkhdyt@proton.me>
2022-12-31 07:36:43 +0700

    test(http): write test for comments endpoint

---
+++ kelas-expert/forum-api/src/Infrastructures/http/_test/comments.test.js
@@ -0,0 +1,120 @@
+const pool = require('../../database/postgres/pool');
+const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelper');
+const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+const createServer = require('../createServer');
+const container = require('../../container');
+
+describe('/threads/{threadId}/comments endpoint', () => {
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  afterEach(async () => {
+    await CommentsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+    await AuthenticationsTableTestHelper.cleanTable();
+  });
+
+  describe('when POST /threads/{threadId}/comments', () => {
+    it('should response 201 and persisted comment', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      await ThreadsTableTestHelper.addThread({});
+      const server = await createServer(container);
+
+      const requestPayload = {
+        content: 'ini konten',
+        owner: 'user-123',
+        threadId: 'thread-123',
+      };
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedComment).toBeDefined();
+    });
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      const server = await createServer(container);
+
+      const requestPayload = {
+        content: 'ini konten',
+        owner: 'user-123',
+      };
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat comment baru karena properti yang dibutuhkan tidak ada'
+      );
+    });
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // arrange
+      const loginPayload = { username: 'tfkhdyt', password: 'secret' };
+      const accessToken = await AuthenticationsTableTestHelper.login(
+        loginPayload
+      );
+      await ThreadsTableTestHelper.addThread({});
+      const server = await createServer(container);
+
+      const requestPayload = {
+        content: 'ini konten',
+        owner: 'user-123',
+        threadId: 123,
+      };
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/threads/thread-123/comments',
+        payload: requestPayload,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      // assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(responseJson.status).toEqual('fail');
+      expect(responseJson.message).toEqual(
+        'tidak dapat membuat comment baru karena tipe data tidak sesuai'
+      );
+    });
+  });
+});
