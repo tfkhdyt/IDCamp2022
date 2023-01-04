68eac0ddbd903701c0a233601aa489213b297f77
tfkhdyt <tfkhdyt@proton.me>
2023-01-04 09:37:07 +0700

    fix(domains/comments): change threadId to owner in validateCommentOwner params

--- kelas-expert/forum-api/src/Domains/comments/CommentRepository.js
+++ kelas-expert/forum-api/src/Domains/comments/CommentRepository.js
@@ -7,7 +7,7 @@ class CommentRepository {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
-  async validateCommentOwner(commentId, threadId) {
+  async validateCommentOwner(commentId, owner) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
