e20de88fdece44209aa9e394d8bca455b58573ab
tfkhdyt <tfkhdyt@proton.me>
2023-01-06 21:18:58 +0700

    feat(comments handler): create "deleteCommentHandler"

--- kelas-expert/forum-api/src/Interfaces/http/api/comments/handler.js
+++ kelas-expert/forum-api/src/Interfaces/http/api/comments/handler.js
@@ -1,10 +1,12 @@
 const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
+const DeleteCommentUseCase = require('../../../../Applications/use_case/DeleteCommentUseCase');
 
 class CommentsHandler {
   constructor(container) {
     this._container = container;
 
     this.postCommentHandler = this.postCommentHandler.bind(this);
+    this.deleteCommentHandler = this.deleteCommentHandler.bind(this);
   }
 
   async postCommentHandler(req, h) {
@@ -30,6 +32,30 @@ class CommentsHandler {
         },
       })
       .code(201);
+
+    return response;
+  }
+
+  async deleteCommentHandler(req, h) {
+    const { threadId, commentId } = req.params;
+
+    const payload = {
+      owner: req.auth.credentials.id,
+      threadId,
+      commentId,
+    };
+
+    const deleteCommentUseCase = this._container.getInstance(
+      DeleteCommentUseCase.name
+    );
+
+    await deleteCommentUseCase.execute(payload);
+
+    const response = h
+      .response({
+        status: 'success',
+      })
+      .code(200);
 
     return response;
   }
