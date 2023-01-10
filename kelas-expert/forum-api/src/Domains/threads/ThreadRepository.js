d792fef0c62d77b287a830b70aed8b1a82360bc2
tfkhdyt <tfkhdyt@proton.me>
2023-01-10 11:09:29 +0700

    feat(thread repo): add findThreadById method

--- kelas-expert/forum-api/src/Domains/threads/ThreadRepository.js
+++ kelas-expert/forum-api/src/Domains/threads/ThreadRepository.js
@@ -6,6 +6,10 @@ class ThreadRepository {
   async validate(threadId) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async findThreadById(threadId) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
