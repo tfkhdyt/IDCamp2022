7703370f8eade8c1c642660b25dc547593c3efcf
tfkhdyt <tfkhdyt@proton.me>
2023-01-12 21:24:10 +0700

    feat(domain error translator): add reply translator

--- kelas-expert/forum-api/src/Commons/exceptions/DomainErrorTranslator.js
+++ kelas-expert/forum-api/src/Commons/exceptions/DomainErrorTranslator.js
@@ -48,6 +48,12 @@ DomainErrorTranslator._directories = {
   'NEW_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError(
     'tidak dapat membuat comment baru karena tipe data tidak sesuai'
   ),
+  'NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError(
+    'tidak dapat membuat reply baru karena properti yang dibutuhkan tidak ada'
+  ),
+  'NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError(
+    'tidak dapat membuat reply baru karena tipe data tidak sesuai'
+  ),
 };
 
 module.exports = DomainErrorTranslator;
--- kelas-expert/forum-api/src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ kelas-expert/forum-api/src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -84,6 +84,25 @@ describe('DomainErrorTranslator', () => {
         'tidak dapat membuat comment baru karena tipe data tidak sesuai'
       )
     );
+
+    expect(
+      DomainErrorTranslator.translate(
+        new Error('NEW_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')
+      )
+    ).toStrictEqual(
+      new InvariantError(
+        'tidak dapat membuat reply baru karena properti yang dibutuhkan tidak ada'
+      )
+    );
+    expect(
+      DomainErrorTranslator.translate(
+        new Error('NEW_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')
+      )
+    ).toStrictEqual(
+      new InvariantError(
+        'tidak dapat membuat reply baru karena tipe data tidak sesuai'
+      )
+    );
   });
 
   it('should return original error when error message is not needed to translate', () => {
