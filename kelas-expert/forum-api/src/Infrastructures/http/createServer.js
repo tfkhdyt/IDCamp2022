98ebf9d04245909d2ffa6a1fc8946186d7b8df1f
tfkhdyt <tfkhdyt@proton.me>
2023-01-13 17:23:10 +0700

    chore(createServer): add console.log debug for response

--- kelas-expert/forum-api/src/Infrastructures/http/createServer.js
+++ kelas-expert/forum-api/src/Infrastructures/http/createServer.js
@@ -82,6 +82,8 @@ const createServer = async (container) => {
         return h.continue;
       }
 
+      console.log({ response });
+
       // penanganan server error sesuai kebutuhan
       const newResponse = h.response({
         status: 'error',
