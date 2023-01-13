732a33ee68e36284341c02059c550fd478ea26fd
tfkhdyt <tfkhdyt@proton.me>
2023-01-13 17:20:18 +0700

    fix(add reply use case): validate thread and comment first

--- kelas-expert/forum-api/src/Applications/use_case/AddReplyUseCase.js
+++ kelas-expert/forum-api/src/Applications/use_case/AddReplyUseCase.js
@@ -1,12 +1,16 @@
 const NewReply = require('../../Domains/replies/entities/NewReply');
 
 class AddReplyUseCase {
-  constructor({ replyRepository }) {
+  constructor({ replyRepository, threadRepository, commentRepository }) {
     this._replyRepository = replyRepository;
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
   }
 
   async execute(useCasePayload) {
     const newReply = new NewReply(useCasePayload);
+    await this._threadRepository.validate(useCasePayload.threadId);
+    await this._commentRepository.validate(useCasePayload.commentId);
     return this._replyRepository.addReply(newReply);
   }
 }
