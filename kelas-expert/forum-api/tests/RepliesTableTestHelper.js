2b28df1a3c36141111e278fefac7eb2b6a9eb07e
tfkhdyt <tfkhdyt@proton.me>
2023-01-13 13:55:40 +0700

    feat(test helper): add replies test helper

---
+++ kelas-expert/forum-api/tests/RepliesTableTestHelper.js
@@ -0,0 +1,43 @@
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+/* istanbul ignore file */
+const RepliesTableTestHelper = {
+  async addReply({
+    id = 'reply-123',
+    content = 'ini balasan',
+    threadId = 'thread-123',
+    commentId = 'comment-123',
+    owner = 'user-123',
+  }) {
+    const query = {
+      text: 'INSERT INTO replies VALUES($1, $2, $3. $4, $5)',
+      values: [id, content, threadId, commentId, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async findReplyById(id) {
+    const query = {
+      text: 'SELECT * FROM replies WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async deleteReply(id) {
+    const query = {
+      text: 'UPDATE replies SET is_deleted = TRUE WHERE id = $1',
+      values: [id],
+    };
+    pool.query(query);
+  },
+
+  async cleanTable() {
+    await pool.query('TRUNCATE TABLE replies CASCADE');
+  },
+};
+
+module.exports = RepliesTableTestHelper;
