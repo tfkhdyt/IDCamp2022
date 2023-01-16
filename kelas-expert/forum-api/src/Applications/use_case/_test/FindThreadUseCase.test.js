cd658e7ed9f2f2a977467d4a2b80c7ff017f45a1
tfkhdyt <tfkhdyt@proton.me>
2023-01-16 18:09:34 +0700

    test(find thread use case): write test for deleted comment and reply

--- kelas-expert/forum-api/src/Applications/use_case/_test/FindThreadUseCase.test.js
+++ kelas-expert/forum-api/src/Applications/use_case/_test/FindThreadUseCase.test.js
@@ -91,6 +91,115 @@ describe('FindCommentUseCase', () => {
 
     // assert
     expect(thread).toStrictEqual(expectedThread);
+    expect(thread.comments[0].content).not.toEqual(
+      '**komentar telah dihapus**'
+    );
+    expect(thread.comments[0].replies[0].content).not.toEqual(
+      '**balasan telah dihapus**'
+    );
+    expect(mockThreadRepository.findThreadById).toBeCalledWith(
+      useCasePayload.threadId
+    );
+    expect(mockCommentRepository.findCommentsByThreadId).toBeCalledWith(
+      useCasePayload.threadId
+    );
+    expect(mockReplyRepository.findRepliesByCommentId).toBeCalledWith(
+      'comment-123'
+    );
+  });
+
+  it('should orchestrating the find thread action correctly, with deleted comment and reply', async () => {
+    // arrange
+    const useCasePayload = {
+      threadId: 'thread-123',
+    };
+    const expectedThread = {
+      ...new AddedThread({
+        id: 'thread-123',
+        title: 'ini title',
+        body: 'ini body',
+        owner: 'user-123',
+        date: expect.any(Date),
+      }),
+      comments: [
+        {
+          id: 'comment-123',
+          username: 'dicoding',
+          content: '**komentar telah dihapus**',
+          date: expect.any(Date),
+          replies: [
+            {
+              id: 'reply-123',
+              username: 'dicoding',
+              content: '**balasan telah dihapus**',
+              date: expect.any(Date),
+            },
+          ],
+        },
+      ],
+    };
+
+    /* creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+    const mockReplyRepository = new ReplyRepository();
+
+    /* mocking needed function */
+    mockThreadRepository.findThreadById = jest.fn().mockImplementation(() =>
+      Promise.resolve(
+        new AddedThread({
+          id: 'thread-123',
+          title: 'ini title',
+          body: 'ini body',
+          owner: 'user-123',
+          date: new Date(),
+        })
+      )
+    );
+    mockCommentRepository.findCommentsByThreadId = jest
+      .fn()
+      .mockImplementation(() =>
+        Promise.resolve([
+          {
+            id: 'comment-123',
+            username: 'dicoding',
+            content: 'ini konten',
+            date: new Date(),
+            is_deleted: true,
+          },
+        ])
+      );
+
+    mockReplyRepository.findRepliesByCommentId = jest
+      .fn()
+      .mockImplementation(() =>
+        Promise.resolve([
+          {
+            id: 'reply-123',
+            username: 'dicoding',
+            content: 'ini konten',
+            date: new Date(),
+            is_deleted: true,
+          },
+        ])
+      );
+
+    /* creating use case instance */
+    const getThreadUseCase = new FindThreadUseCase({
+      threadRepository: mockThreadRepository,
+      commentRepository: mockCommentRepository,
+      replyRepository: mockReplyRepository,
+    });
+
+    // action
+    const thread = await getThreadUseCase.execute(useCasePayload.threadId);
+
+    // assert
+    expect(thread).toStrictEqual(expectedThread);
+    expect(thread.comments[0].content).toEqual('**komentar telah dihapus**');
+    expect(thread.comments[0].replies[0].content).toEqual(
+      '**balasan telah dihapus**'
+    );
     expect(mockThreadRepository.findThreadById).toBeCalledWith(
       useCasePayload.threadId
     );
