9dc779dde27c89b84c38e3e7159908ac411d673c
tfkhdyt <tfkhdyt@proton.me>
2023-01-30 10:50:21 +0700

    feat(domains): add likes domains

---
+++ kelas-expert/forum-api/src/Domains/likes/LikeRepository.js
@@ -0,0 +1,15 @@
+class LikeRepository {
+  async likeComment(newLike) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async unlikeComment(newLike) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async checkLikeStatus(newLike) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+}
+
+module.exports = LikeRepository;
---
+++ kelas-expert/forum-api/src/Domains/likes/_test/LikeRepository.test.js
@@ -0,0 +1,19 @@
+const LikeRepository = require('../LikeRepository');
+
+describe('LikeRepository interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // arrange
+    const likeRepository = new LikeRepository();
+
+    // action and assert
+    await expect(likeRepository.likeComment({})).rejects.toThrowError(
+      'LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
+    await expect(likeRepository.unlikeComment({})).rejects.toThrowError(
+      'LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
+    await expect(likeRepository.checkLikeStatus({})).rejects.toThrowError(
+      'LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+    );
+  });
+});
